bc9e7d96d660b3054c97c155c2ddea7849e65db3
violva317018 <violva317018@gamil.com>
2023-08-11 13:50:14 +0800

    header做RWD+漢堡

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -52,93 +52,123 @@ function Header() {
   // console.log(JSON.stringify(localStorage.getItem("userInfo")));
   return (
     <div className="header">
-      <div className="h50 d-flex my-auto align-items-center">
-        <Link to="/" className="mx-5">
-          <img src={headerLogo} width="200" height="100%" alt="img" />
-        </Link>
-        <Link
-          to="/proposal"
-          className="mx-5 proposal-div"
-          style={{ lineHeight: "70px" }}
+      <div className="h50 d-flex my-auto align-items-center navbar-expand-lg navbar-light fW">
+        <div className="LOGO">
+          <Link to="/" className="mx-5 logoIMG">
+            <img src={headerLogo} width="200" height="100%" alt="img" />
+          </Link>
+        </div>
+        <button
+          className="navbar-toggler hbg ms-auto"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          提案
-        </Link>
-        <Link to="/allCase" className="mx-5 allCase-div">
-          接案
-        </Link>
-        {userinfo ? (
-          <div className="dropdown ms-auto">
-            <Link
-              to="#"
-              className="d-block link-dark text-decoration-none"
-              id="dropdownUser2"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <img
-                src={headphoto}
-                alt="mdo"
-                width="40"
-                height="40"
-                className="rounded-circle"
-              />
-            </Link>
-            <ul
-              className="dropdown-menu text-small shadow"
-              aria-labelledby="dropdownUser2"
-            >
-              <li>
-                <Link
-                  className="dropdown-item"
-                  to="/personalinfo"
-                  onClick={() => setInfoData("1")}
-                >
-                  我的帳號
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="dropdown-item"
-                  to="/personalinfo"
-                  onClick={() => setInfoData("3")}
-                >
-                  我的案件
-                </Link>
-              </li>
-              <li>
-                <Link className="dropdown-item" to="/chatRoom">
-                  聊天室
-                  <span
-                    className={
-                      unreadNotifications?.length === 0
-                        ? null
-                        : "chatRoomNotification"
-                    }
-                  ></span>
-                </Link>
-              </li>
-              <li>
-                <hr className="dropdown-divider" />
-              </li>
-              <li>
-                <Link className="dropdown-item" to="" onClick={handleLogout}>
-                  登出
-                </Link>
-              </li>
-            </ul>
-          </div>
-        ) : (
-          <div className="d-flex ms-auto">
-            {/* 第一個標籤不能運作的BUG，所要加一個DIV */}
-            <div></div>
-            <Link to="/login" className="mx-2">
-              登入
-            </Link>
-            <Link to="/register" className="mx-2">
-              註冊
-            </Link>
-          </div>
-        )}
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse show1"
+          id="navbarSupportedContent"
+        >
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0 hbgUl d-flex my-auto align-items-center">
+            <li class="nav-item">
+              <Link to="/proposal" className="headerBTN1">
+                提案
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link to="/allCase" className="headerBTN2">
+                接案
+              </Link>
+            </li>
+            <li class="nav-item dFlex">
+              {userinfo ? (
+                <div className="dropdown ms-auto">
+                  <Link
+                    to="#"
+                    className="d-block link-dark text-decoration-none"
+                    id="dropdownUser2"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    <img
+                      src={headphoto}
+                      alt="mdo"
+                      width="40"
+                      height="40"
+                      className="rounded-circle rwdPhoto"
+                    />
+                  </Link>
+                  <ul
+                    className="dropdown-menu text-small shadow rwdhead"
+                    aria-labelledby="dropdownUser2"
+                  >
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to="/personalinfo"
+                        onClick={() => setInfoData("1")}
+                      >
+                        我的帳號
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to="/personalinfo"
+                        onClick={() => setInfoData("3")}
+                      >
+                        我的案件
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/chatRoom">
+                        聊天室
+                        <span
+                          className={
+                            unreadNotifications?.length === 0
+                              ? null
+                              : "chatRoomNotification"
+                          }
+                        ></span>
+                      </Link>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to=""
+                        onClick={handleLogout}
+                      >
+                        登出
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              ) : (
+                <div className="dFlex">
+                  {/* 第一個標籤不能運作的BUG，所要加一個DIV */}
+                  <div></div>
+                  <div className="loginBC">
+                    <Link to="/login" className="loginBCHover">
+                      登入
+                    </Link>
+                  </div>
+                  <div className="RegisterBC">
+                    <Link to="/register" className="loginBCHover">
+                      註冊
+                    </Link>
+                  </div>
+                </div>
+              )}
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
