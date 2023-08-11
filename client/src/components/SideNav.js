748d725ffdbc80e26207fd021e07b484214a8508
violva317018 <violva317018@gamil.com>
2023-08-11 16:37:37 +0800

    改sidenav

--- client/src/components/SideNav.js
+++ client/src/components/SideNav.js
@@ -7,33 +7,45 @@ function SideNav() {
   const { setInfoData, setProposal, setTakethecase } = useContext(GlobelDate);
 
   return (
-    <div className="sidenav">
-      <p>我的帳戶</p>
-      <hr />
-      <Link to="" onClick={() => setInfoData("1")}>
-        帳戶資料
-      </Link>
-      <Link to="" onClick={() => setInfoData("2")}>
-        我的收藏
-      </Link>
-      <Link
-        to=""
-        onClick={() => {
-          setInfoData("3");
-          setProposal("1");
-        }}
+    <div>
+      <button
+        className="btn btn-primary "
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#collapseExample"
+        aria-expanded="false"
+        aria-controls="collapseExample"
       >
-        提案紀錄
-      </Link>
-      <Link
-        to=""
-        onClick={() => {
-          setInfoData("4");
-          setTakethecase("1");
-        }}
-      >
-        接案紀錄
-      </Link>
+        &gt;
+      </button>
+      <div className="sidenav collapse" id="collapseExample">
+        <p>我的帳戶</p>
+        <hr />
+        <Link to="" onClick={() => setInfoData("1")}>
+          帳戶資料
+        </Link>
+        <Link to="" onClick={() => setInfoData("2")}>
+          我的收藏
+        </Link>
+        <Link
+          to=""
+          onClick={() => {
+            setInfoData("3");
+            setProposal("1");
+          }}
+        >
+          提案紀錄
+        </Link>
+        <Link
+          to=""
+          onClick={() => {
+            setInfoData("4");
+            setTakethecase("1");
+          }}
+        >
+          接案紀錄
+        </Link>
+      </div>
     </div>
   );
 }
