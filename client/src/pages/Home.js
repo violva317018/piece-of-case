6de88472e7369bc8f7b0725b1383e30ee50f48e9
violva317018 <violva317018@gamil.com>
2023-08-14 16:41:50 +0800

    修改home主頁未完

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -43,10 +43,14 @@ function Home() {
       <main>
         <div className="d-flex justify-content-evenly homeText">
           <Link to="/proposal" className="d-block">
-            <div className="get-case case-box">提案</div>
+            <div className="frameBox">
+              <div className="get-case case-box">提案</div>
+            </div>
           </Link>
           <Link to="/allCase">
-            <div className="send-case case-box">接案</div>
+            <div className="frameBox">
+              <div className="send-case case-box">接案</div>
+            </div>
           </Link>
         </div>
         <div className="homeText fs-1 text-center">最方便、快速的接案平台</div>
